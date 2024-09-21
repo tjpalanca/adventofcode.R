32fc11c8f62ade2739ea6f5ba41837a4a179326e
TJ Palanca <mail@tjpalanca.com>
2024-09-21 13:04:12 +0000

    Day 5 Part 1

--- 2023/day05.R
+++ 2023/day05.R
@@ -7,16 +7,9 @@ library(dplyr)
 library(tidyr)
 library(purrr)
 
-text <- readr::read_lines("2023/day05.txt")
+# Preparation -------------------------------------------------------------
 
-seeds.df <-
-  tibble(
-    type = "seed",
-    index = text[1] %>%
-      str_split_1(": ") %>%
-      extract2(2) %>%
-      str_split_1(" ")
-  )
+text <- read_lines("2023/day05-sample.txt")
 
 mappings.df <-
   tibble(text = text[3:length(text)]) %>%
@@ -26,29 +19,61 @@ mappings.df <-
   summarise(text = list(text)) %>%
   mutate(title = map_chr(text, 1L) %>% str_remove(" map:$")) %>%
   transmute(
-    source = str_extract(title, "^([a-z]+)-to-", group = 1L),
-    destination = str_extract(title, "-to-([a-z]+)$", group = 1L),
+    src = str_extract(title, "^([a-z]+)-to-", group = 1L),
+    dst = str_extract(title, "-to-([a-z]+)$", group = 1L),
     mapping = map(text, function(text) {
       tibble(text = text[2L:length(text)]) %>%
         separate(text, c("dst_lower", "src_lower", "length"), sep = " ") %>%
         mutate_all(as.numeric) %>%
-        mutate(dst_upper = dst_lower + length,
-               src_upper = src_lower + length)
+        mutate(dst_upper = dst_lower + length - 1,
+               src_upper = src_lower + length - 1)
     })
+  ) %>%
+  unnest(mapping) %>%
+  select(src, dst, src_lower, src_upper, dst_lower, dst_upper)
+
+maps.df <-
+  mappings.df %>%
+  distinct(src, dst)
+
+# Part 1 ------------------------------------------------------------------
+
+seeds.df <-
+  tibble(
+    type = "seed",
+    index = text[1] %>%
+      str_split_1(": ") %>%
+      extract2(2) %>%
+      str_split_1(" ") %>%
+      as.numeric()
   )
+
+map_to_next <- function(seeds, mappings) {
+
+  seeds %>%
+    left_join(
+      mappings,
+      by = join_by(
+        type == src,
+        between(index, src_lower, src_upper)
+      )
+    ) %>%
+    transmute(
+      type = first(dst, na_rm = TRUE),
+      index = coalesce(index - src_lower + dst_lower, index)
+    )
+
+}
 
 seeds.df %>%
-  left_join(mappings.df, by=c("type" = "source")) ->
-  A
+  map_to_next(mappings.df) %>%
+  map_to_next(mappings.df) %>%
+  map_to_next(mappings.df) %>%
+  map_to_next(mappings.df) %>%
+  map_to_next(mappings.df) %>%
+  map_to_next(mappings.df) %>%
+  map_to_next(mappings.df) ->
+  locations.df
 
-index <- A$index[1]
-mapping <- A$mapping[[1]]
+min(locations.df$index)
 
-mapping %>%
-  left_join()
-
-
-  transmute(
-    type = destination,
-    index = map_to_destination(index, mapping)
-  )
